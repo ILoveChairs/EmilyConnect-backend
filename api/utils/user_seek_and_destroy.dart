7ccd052df77a43f202fad21b8ad6308b3f765eb8
Matias Davezac <8675@holbertonstudents.com>
2024-10-10 08:55:10 -0300

    Updated user seek

--- api/utils/user_seek_and_destroy.dart
+++ api/utils/user_seek_and_destroy.dart
@@ -1,13 +1,30 @@
 
 
+import 'dart:async';
+
+import 'package:dart_firebase_admin/firestore.dart';
+
 import './firebase.dart';
 import './firestore_names.dart';
 
 
-Future<void> userSeekAndDestroy(String ci) async {
-  final courses = await firestore.collection(coursesCollection).get();
-  for (final course in courses.docs) {
-    final path = course.ref.path;
-    await firestore.collection('$path/Students').doc(ci).delete();
-  }
+Future<List<DocumentReference<Map<String, Object?>>>> userSeek(String ci) async {
+  return (await firestore.collection(coursesCollection).get())
+    .docs.map((doc) => firestore.collection('${doc.ref.path}/Students').doc(ci))
+    .toList();
 }
+
+
+Future<void> userSeekAndDestroy(String ci) async {
+  final courses = await userSeek(ci);
+  for (final doc in courses) {
+    unawaited(doc.delete());
+  }}
+
+
+Future<void> userSeekAndUpdate(String ci, Map<String, dynamic> data) async {
+  final courses = await userSeek(ci);
+  for (final doc in courses) {
+    unawaited(doc.update(data));
+  }}
+  
